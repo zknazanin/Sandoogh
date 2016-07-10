b1f3e87f66a397862a8bb4ed4bebb3fb2b9548fc
Zahra Kakavand <zk.nazanin@gmail.com>
2016-07-10 18:27:29 +0430

    Project Initialized

---
+++ app/src/test/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package io.sharif.prj.st91106224.st91105693.st91106235.sandoogh;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
