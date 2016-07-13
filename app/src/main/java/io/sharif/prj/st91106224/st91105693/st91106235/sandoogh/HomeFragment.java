278b4398b88c4dfc389f3b69425eabe75384f10b
foroughM <fmirkarimzade@gmail.com>
2016-07-13 20:59:58 +0430

    sandogh page added

--- app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/HomeFragment.java
+++ app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/HomeFragment.java
@@ -1,8 +1,11 @@
 package io.sharif.prj.st91106224.st91105693.st91106235.sandoogh;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -23,21 +26,19 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         ViewGroup view = (ViewGroup) inflater.inflate(R.layout.home_fragment, container, false);
-//        LinearLayout rl = (LinearLayout)view.findViewById(R.id.ground);
-//        Button[] b = new Button[3];
-//        for (int i=0 ; i<3 ; i++){
-//            b[i] = new Button(getContext());
-//            LinearLayout.LayoutParams btnParams = new LinearLayout.LayoutParams(
-//                    AbsListView.LayoutParams.WRAP_CONTENT, AbsListView.LayoutParams.WRAP_CONTENT);
-//            b[i].setText(R.string.app_name);
-//            b[i].setGravity(Gravity.CENTER);
-//            b[i].setLayoutParams(btnParams);
-//            rl.addView(b[i]);
-//        }
         String[] Values = {"sandoogh 1","sandoogh 2", "sandoogh 3"};
         SandooghArrayAdapter adapter = new SandooghArrayAdapter(getActivity(),Values);
         ListView lv = (ListView)view.findViewById(R.id.list);
         lv.setAdapter(adapter);
+
+        // Setup Toolbar
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.tool_bar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
+        // Get current user from Firebase
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        user = firebaseAuth.getCurrentUser();
+
         return view;
     }
 }
@@ -61,23 +62,5 @@ class SandooghArrayAdapter extends ArrayAdapter<String> {
         Button btn = (Button) rowView.findViewById(R.id.buttonDy);
         btn.setText(values[position]);
         return rowView;
-
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.home_fragment, container, false);
-
-        // Setup Toolbar
-        Toolbar toolbar = (Toolbar) view.findViewById(R.id.tool_bar);
-        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
-
-        // Get current user from Firebase
-        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
-        user = firebaseAuth.getCurrentUser();
-
-        TextView textView = (TextView) view.findViewById(R.id.textview);
-
-        if (user != null) {
-            textView.setText(user.getEmail());
-        }
-        return view;
     }
 }
