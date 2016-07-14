58a7ab69c9e2d85536586ae717b226708cc1ce2e
foroughM <fmirkarimzade@gmail.com>
2016-07-14 11:44:31 +0430

    sandogh page added

---
+++ app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/fragments/sanType.java
@@ -0,0 +1,19 @@
+package io.sharif.prj.st91106224.st91105693.st91106235.sandoogh.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import io.sharif.prj.st91106224.st91105693.st91106235.sandoogh.R;
+
+public class sanType extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.another_fragment, container, false);
+    }
+}
