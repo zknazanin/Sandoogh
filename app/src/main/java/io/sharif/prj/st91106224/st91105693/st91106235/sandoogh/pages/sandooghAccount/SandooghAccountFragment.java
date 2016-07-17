781e441d7c35eb4bc86d6ae56adfb24eb34a0b04
Shamim <sh.karimi94@gmail.com>
2016-07-17 12:27:29 +0430

    Sandoogh account fragment updated.

--- app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/pages/sandooghAccount/SandooghAccountFragment.java
+++ app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/pages/sandooghAccount/SandooghAccountFragment.java
@@ -2,6 +2,7 @@ package io.sharif.prj.st91106224.st91105693.st91106235.sandoogh.pages.sandooghAc
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.PopupMenu;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -24,6 +25,7 @@ public class SandooghAccountFragment extends Fragment {
     ImageView menu;
     ViewGroup view;
     EditText temp;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -34,8 +36,11 @@ public class SandooghAccountFragment extends Fragment {
             view = (ViewGroup) inflater.inflate(R.layout.san_account_a, container, false);
         } else {
             view = (ViewGroup) inflater.inflate(R.layout.san_account_b, container, false);
-
         }
+
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.tool_bar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+
         createMenu();
 
         temp = (EditText) view.findViewById(R.id.san_AccNum_edit);
@@ -43,43 +48,43 @@ public class SandooghAccountFragment extends Fragment {
         temp = (EditText) view.findViewById(R.id.san_CardNum_edit);
         temp.setText(sandoogh.getCardNum());
         temp = (EditText) view.findViewById(R.id.member_num_edit);
-        temp.setText(sandoogh.getMembers().length);
+        temp.setText(String.valueOf(sandoogh.getMemberIds().size()));
         temp = (EditText) view.findViewById(R.id.san_Period_edit);
         temp.setText(sandoogh.getPeriod());
         temp = (EditText) view.findViewById(R.id.san_amount_edit);
-        temp.setText(sandoogh.getPeriodPay());
+        temp.setText(String.valueOf(sandoogh.getPeriodPay()));
         temp = (EditText) view.findViewById(R.id.start_date_edit);
         temp.setText(sandoogh.getStartDate().toString());
         if (sandoogh.getType().equals("A")) {
             temp = (EditText) view.findViewById(R.id.san_total_edit);
-            temp.setText(sandoogh.getTotal());
+            temp.setText(String.valueOf(sandoogh.getTotal()));
         }
         return view;
     }
 
     private void createMenu() {
-        menu = (ImageView) view.findViewById(R.id.popup);
-        menu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                PopupMenu popup = new PopupMenu(getContext(), menu);
-                popup.getMenuInflater()
-                        .inflate(R.menu.san_popup, popup.getMenu());
-                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    public boolean onMenuItemClick(MenuItem item) {
-                        if (item.getTitle().equals(getString(R.string.san_account_edit))) {
-
-                        } else if (item.getTitle().equals(getString(R.string.san_account_amount))) {
-
-                        } else {
-                            // show member
-                        }
-                        return true;
-                    }
-                });
-
-                popup.show();
-            }
-        });
+//        menu = (ImageView) view.findViewById(R.id.popup);
+//        menu.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                PopupMenu popup = new PopupMenu(getContext(), menu);
+//                popup.getMenuInflater()
+//                        .inflate(R.menu.san_popup, popup.getMenu());
+//                popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+//                    public boolean onMenuItemClick(MenuItem item) {
+//                        if (item.getTitle().equals(getString(R.string.san_account_edit))) {
+//
+//                        } else if (item.getTitle().equals(getString(R.string.san_account_amount))) {
+//
+//                        } else {
+//                            // show member
+//                        }
+//                        return true;
+//                    }
+//                });
+//
+//                popup.show();
+//            }
+//        });
     }
 }
