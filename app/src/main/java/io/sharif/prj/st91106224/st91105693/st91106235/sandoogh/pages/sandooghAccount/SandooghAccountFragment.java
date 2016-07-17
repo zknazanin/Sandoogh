156c65f6b0ae975ab1fcf4f46ba1df1c566a3136
foroughM <fmirkarimzade@gmail.com>
2016-07-17 11:23:40 +0430

    sandoogh account

--- app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/pages/sandooghAccount/SandooghAccountFragment.java
+++ app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/pages/sandooghAccount/SandooghAccountFragment.java
@@ -3,17 +3,12 @@ package io.sharif.prj.st91106224.st91105693.st91106235.sandoogh.pages.sandooghAc
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.PopupMenu;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import android.widget.ImageView;
-
-import java.lang.reflect.Field;
 
 import io.sharif.prj.st91106224.st91105693.st91106235.sandoogh.R;
 import io.sharif.prj.st91106224.st91105693.st91106235.sandoogh.data.Sandoogh;
@@ -22,7 +17,7 @@ import io.sharif.prj.st91106224.st91105693.st91106235.sandoogh.data.Sandoogh;
 public class SandooghAccountFragment extends Fragment {
     ImageView menu;
     ViewGroup view;
-
+    EditText temp;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 
@@ -35,9 +30,24 @@ public class SandooghAccountFragment extends Fragment {
             view = (ViewGroup) inflater.inflate(R.layout.san_account_b, container, false);
 
         }
-
-
         createMenu();
+
+        temp = (EditText) view.findViewById(R.id.san_AccNum_edit);
+        temp.setText(sandoogh.getAccountNum());
+        temp = (EditText) view.findViewById(R.id.san_CardNum_edit);
+        temp.setText(sandoogh.getCardNum());
+        temp = (EditText) view.findViewById(R.id.member_num_edit);
+        temp.setText(sandoogh.getMembers().length);
+        temp = (EditText) view.findViewById(R.id.san_Period_edit);
+        temp.setText(sandoogh.getPeriod());
+        temp = (EditText) view.findViewById(R.id.san_amount_edit);
+        temp.setText(sandoogh.getPeriodPay());
+        temp = (EditText) view.findViewById(R.id.start_date_edit);
+        temp.setText(sandoogh.getStartDate().toString());
+        if (sandoogh.getType().equals("A")) {
+            temp = (EditText) view.findViewById(R.id.san_total_edit);
+            temp.setText(sandoogh.getTotal());
+        }
         return view;
     }
 
