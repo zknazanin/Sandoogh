4ff8c02df25f01d5988c0f472200efdcc8ed59f9
Zahra Kakavand <zk.nazanin@gmail.com>
2016-07-26 16:23:37 +0430

    profile image in circle

--- app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/pages/home/MainActivity.java
+++ app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/pages/home/MainActivity.java
@@ -4,7 +4,16 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
@@ -174,6 +183,10 @@ public class MainActivity extends AppCompatActivity {
         drawerList = (ListView) findViewById(R.id.left_drawer);
         View header = getLayoutInflater().inflate(R.layout.header, null);
         final ImageView imageView = (ImageView) header.findViewById(R.id.img_first);
+
+        Bitmap bitmap = ((BitmapDrawable)imageView.getDrawable()).getBitmap();
+        imageView.setImageBitmap(getCircleBitmap(bitmap));
+
         final TextView textView = (TextView) header.findViewById(R.id.textView);
         try {
             firebaseAuth = FirebaseAuth.getInstance();
@@ -188,6 +201,7 @@ public class MainActivity extends AppCompatActivity {
                         imageView.setImageBitmap(
                                 BitmapFactory.decodeByteArray(imageAsBytes, 0, imageAsBytes.length)
                         );
+
                     }
                 }
 
@@ -240,4 +254,30 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    public static Bitmap getCircleBitmap(Bitmap bitmap) {
+        final Bitmap circuleBitmap = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getWidth(), Bitmap.Config.ARGB_8888);
+        final Canvas canvas = new Canvas(circuleBitmap);
+
+        final int color = Color.RED;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getWidth());
+        final RectF rectF = new RectF(rect);
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawOval(rectF, paint);
+
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        bitmap.recycle();
+
+        return circuleBitmap;
+    }
+
 }
+
+
+
