ca437faa71d4ae95064760c234c60fa79af81664
Shamim <sh.karimi94@gmail.com>
2016-07-27 13:21:04 +0430

    Admin Payment Report done.

--- app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/pages/sandooghAccount/Admin/AdminPaymentAdapter.java
+++ app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/pages/sandooghAccount/Admin/AdminPaymentAdapter.java
@@ -6,13 +6,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
-import java.util.List;
+import java.util.ArrayList;
 
-import io.sharif.prj.st91106224.st91105693.st91106235.sandoogh.data.Payment;
+import io.sharif.prj.st91106224.st91105693.st91106235.sandoogh.data.UserPayment;
 
-public class AdminPaymentAdapter extends ArrayAdapter<Payment> {
+public class AdminPaymentAdapter extends ArrayAdapter<UserPayment> {
 
-    public AdminPaymentAdapter(Context c, List<Payment> items) {
+    public AdminPaymentAdapter(Context c, ArrayList<UserPayment> items) {
         super(c, 0, items);
     }
 
--- app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/pages/sandooghAccount/Admin/AdminPaymentView.java
+++ app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/pages/sandooghAccount/Admin/AdminPaymentView.java
@@ -7,15 +7,25 @@ import android.view.ViewGroup;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
 import io.sharif.prj.st91106224.st91105693.st91106235.sandoogh.R;
-import io.sharif.prj.st91106224.st91105693.st91106235.sandoogh.data.Payment;
+import io.sharif.prj.st91106224.st91105693.st91106235.sandoogh.data.User;
+import io.sharif.prj.st91106224.st91105693.st91106235.sandoogh.data.UserPayment;
 
 public class AdminPaymentView extends RelativeLayout {
 
     Context context;
 
-    private TextView deadlineTextView;
-    private TextView amountTextView;
+    private TextView usernameTextView;
+    private TextView idTextView;
+    private TextView approvedTextView;
 
     public AdminPaymentView(Context c) {
         this(c, null);
@@ -38,13 +48,35 @@ public class AdminPaymentView extends RelativeLayout {
     }
 
     private void setupChildren() {
-        deadlineTextView = (TextView) findViewById(R.id.payment_deadline_text_view);
-        amountTextView = (TextView) findViewById(R.id.payment_amount_text_view);
+        usernameTextView = (TextView) findViewById(R.id.payment_username_text_view);
+        idTextView = (TextView) findViewById(R.id.payment_id_text_view);
+        approvedTextView = (TextView) findViewById(R.id.payment_approved_text_view);
     }
 
-    public void setItem(Payment payment) {
-        amountTextView.setText(String.valueOf(payment.getAmount()));
-        deadlineTextView.setText(payment.getDeadline().toString());
+    public void setItem(UserPayment userPayment) {
+
+        idTextView.setText(userPayment.getPaymentID());
+        if (userPayment.isApproved()) {
+            approvedTextView.setText(R.string.payment_report_dialog_approved);
+        } else {
+            approvedTextView.setText(R.string.payment_report_dialog_not_approved);
+        }
+
+        FirebaseDatabase.getInstance().getReference().child("Users").child(userPayment.getUserID()).addListenerForSingleValueEvent(
+                new ValueEventListener() {
+
+                    @Override
+                    public void onDataChange(DataSnapshot snapshot) {
+                        User user = snapshot.getValue(User.class);
+                        usernameTextView.setText(user.getUsername());
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
+
     }
 
 }
