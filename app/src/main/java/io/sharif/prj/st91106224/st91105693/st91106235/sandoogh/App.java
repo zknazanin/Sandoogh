a4cc46fa681689c8ce6b5456083c304eab15c4cc
foroughM <fmirkarimzade@gmail.com>
2016-07-27 12:42:25 +0430

    pic

--- app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/App.java
+++ app/src/main/java/io/sharif/prj/st91106224/st91105693/st91106235/sandoogh/App.java
@@ -2,8 +2,6 @@ package io.sharif.prj.st91106224.st91105693.st91106235.sandoogh;
 
 import android.app.Application;
 
-import com.google.firebase.database.FirebaseDatabase;
-
 
 public class App extends Application {
 
@@ -11,7 +9,7 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        FirebaseDatabase.getInstance().setPersistenceEnabled(true);
+     //   FirebaseDatabase.getInstance().setPersistenceEnabled(true);
     }
 
 }
